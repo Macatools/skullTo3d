a2b57fc612af7c50d1c1315ecd6c2c274b787eeb
David Meunier <david_meunier_79@hotmail.fr>
2024-06-19 15:17:37 +0200

    breaking

--- workflows/segment_petra.py
+++ workflows/segment_petra.py
@@ -856,7 +856,7 @@ def create_main_workflow(data_dir, process_dir, soft, species, subjects,
                                     or "quick_angio_pipe" in params.keys()):
 
             print("rename_all_angio_derivatives")
-
+            0/0
             rename_all_angio_derivatives(params, main_workflow, angio_pipe,
                                          datasink, pref_deriv, parse_str)
 
