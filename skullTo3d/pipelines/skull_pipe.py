46ad9faeae4045a7b2366d0b0e711650b7b22a36
David Meunier <david_meunier_79@hotmail.fr>
2024-10-11 10:29:11 +0200

    rigid

--- skullTo3d/pipelines/skull_pipe.py
+++ skullTo3d/pipelines/skull_pipe.py
@@ -387,7 +387,7 @@ def create_skull_ct_pipe(name="skull_ct_pipe", params={}):
         interface=RegResample(pad_val=0.0),
         name="align_ct_on_stereo_T1")
 
-    align_ct_on_T1.inputs.align_ct_on_stereo_T1 = True
+    align_ct_on_stereo_T1.inputs.rig_only_flag= True
 
     skull_ct_pipe.connect(align_ct_on_T1, 'res_file',
                           align_ct_on_stereo_T1, "flo_file")
