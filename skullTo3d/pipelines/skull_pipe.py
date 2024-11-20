55fa90840690a85c1e67c8f1205a96986f8b1761
David Meunier <david_meunier_79@hotmail.fr>
2024-11-20 17:39:39 +0100

    petra_skull_auto_mask with indiv (also in previsou commit)

--- skullTo3d/pipelines/skull_pipe.py
+++ skullTo3d/pipelines/skull_pipe.py
@@ -778,7 +778,7 @@ def create_skull_petra_pipe(name="skull_petra_pipe", params={}):
                                  petra_skull_auto_mask, "img_file")
 
         skull_petra_pipe.connect(
-            inputnode, ('indiv_params', parse_key, "petra_fast"),
+            inputnode, ('indiv_params', parse_key, "petra_skull_auto_mask"),
             petra_skull_auto_mask, "indiv_params")
 
     elif "petra_skull_mask_thr" in params.keys():
