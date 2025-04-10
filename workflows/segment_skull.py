a98c655c78a03f9e309fb5d872c7b60f7ad8809d
David Meunier <david_meunier_79@hotmail.fr>
2025-04-10 14:41:20 +0200

    dump

--- workflows/segment_skull.py
+++ workflows/segment_skull.py
@@ -993,7 +993,7 @@ def create_main_workflow(cmd, data_dir, process_dir, soft, species, subjects,
             counter += 1
 
     with open(real_params_file, 'w+') as fp:
-        json.dumps(params, fp, sort_keys=True, indent=4)
+        json.dump(params, fp, sort_keys=True, indent=4)
 
     if deriv:
         try:
